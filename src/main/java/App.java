ec2b398c0c796c607ba435ae097c30cd41b2a32e
nikita <mikropyl28@gmail.com>
2020-11-12 02:16:09 +0300

    ?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorldBean1.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean2.getMessage());
+        System.out.println(helloWorldBean1.equals(helloWorldBean2));
+        Cat  catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+        System.out.println(catBean1.equals(catBean2));
     }
 }
